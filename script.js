e9448eb76d2dc01ef5790dd11f777f41e6faab3e
shah <emonshah19446@gmail.com>
2020-03-18 11:54:11 -0400

    final cahnges

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
 const characterAmountrange = document.getElementById
     ('characterAmountrange')
-const characterAmountNumbers = document.getElementById
-    ('CharacterAmountNumbers')
-characterAmountNumbers.addEventListener('input', syncNumbers)
-characterAmountrange.addEventListener('input', syncAmount)
+const characterAmountNumber = document.getElementById
+    ('characterAmountNumber')
+const form = document.getElementById('passwordgeneratorForm')
+const UPPER_CASE_CHAR_CODES = arrayFromLowtoHigh(65, 90)
+const LOWER_CASE_CHAR_CODES = arrayFromLowtoHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowtoHigh(48, 67)
+const SPECIAL_CHAR_CODES = arrayFromLowtoHigh(33, 47).concat(
+    arrayFromLowtoHigh(58, 64)
+)
+const uppercase = document.getElementById
+"uppercase"
+//characterAmountNumber.addEventListener('input', SyncCharacterAmount)
+characterAmountrange.addEventListener('input', SyncCharacterAmount)
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const characterAmountNumber = characterAmountNumber.value
+    const uppercase = uppercaseElement.checked
+    const Numbers = NumbersElement.checked
+    const lowercase = lowercaseElement.checked
+    const specialcharacters = specialcharactersElement.checked
 
 
-function SyncAmount(e) {
-    const value = e.target.value
-    characterAmountNumbers.value = value
-    characterAmountrange.value = value
+
+
+})
+
+function generatepassword(Amountofcharacters, uppercase, lowercase, specialcharacters, Numbers) {
+    String.fromCharCode(65)
+    let charcodes = LOWER_CHAR_CODES
+    if (uppercase) charcodes = charcode.concat(UPPER_CASE_CHAR_CODES)
+    if (Numbers) charcodes = charcode.concat(NUMBER_CHAR_CODES)
+    if (specialcharacters) charcode = harcode.concat(SPECIAL_CHAR_CODES)
 }
+function arrayFromLowtoHigh(low, high) {
+    const array = []
+    for (let i = low; i <= high; i++) {
+        array.push(i)
+    }
+    return array
+
+}
+
+function SyncCharacterAmount(e) {
+    const value = e.target.value
+
+    //characterAmountNumber.value = value
+    characterAmountrange.value = value
+
+}
+
