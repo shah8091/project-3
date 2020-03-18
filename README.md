33069031b025be294572e59c5e3d19f1b74d3b50
shah <emonshah19446@gmail.com>
2020-03-17 23:00:02 -0400

    new

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ image // html validator// image whats is this repo
 
 This  is a password generator code. this application generates pasword based on the input of the users required inputs are
 what length do you want the password to be?
-do you need lower cases?
-do you need upper cases?
-do you need numeric characters?
-do you need special characters?
-after all of the inputs are added to the propmt, the script will generate a random password based on the input. what is the purpose of this repo
+user can selct the amount of numbers on "range meter"
+user should be able to check on the check box for upper cases.
+user should be able to check on the check box fror lower cases.
+user should be able to check on the check box for the special charactars.
+after all of the inputs are added to the propmt, the script will generate a random password based on the input. what is the purpose of this repo?
 the purpose of this repo is to create a random password generator what is it for
 this is the third assignment, where we are learning and implementing javascript code to greate a random password generator. links
--- script.js
+++ script.js
@@ -1,64 +1,13 @@
+const characterAmountrange = document.getElementById
+    ('characterAmountrange')
+const characterAmountNumbers = document.getElementById
+    ('CharacterAmountNumbers')
+characterAmountNumbers.addEventListener('input', syncNumbers)
+characterAmountrange.addEventListener('input', syncAmount)
 
 
-function passGenerator() {
-    // Alert 1
-    // created a variable to hold the length
-    var length = prompt("Select the length of the passward: (between 8 to 128)");
-    //converted string input to integer
-    var passLength = parseInt(length);
-    console.log(parseInt(passLength));
-
-    // created an empty variable to generate password from
-    let value = "";
-
-    // Alert 2
-    // created a boolean variable for user choice if he wants to include upper case
-    var upCase = confirm("Include UpperCase? ");
-    // if true, added all capital letter into value
-    if (upCase == true) {
-        value = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    }
-    console.log(value);
-
-    //Alert 3
-    // created a boolean variable for user choice if he wants to include lower case
-    var lowCase = confirm("Include LowerCase? ");
-    // if true, concate all small letter into value
-    if (lowCase == true) {
-        value = value.concat("abcdefghijklmnopqrstuvwxyz");
-    }
-    console.log(value);
-
-    // Alert 4
-    //created a boolean variable for user choice if he wants to inclue number
-    var num = confirm("Include Numeric? ");
-    // if true, concate all the number with value
-    if (num == true) {
-        value = value.concat("0123456789");
-    }
-    console.log(value);
-
-    // Alert 5
-    // created a boolean variable for user choice if he wants to include special charcter into value
-    var char = confirm("Include Special Character? ");
-    // if true, concate all the special charcter with value
-    if (lowCase == true) {
-        value = value.concat("!@#$%^&*()_+");
-    }
-    console.log(value);
-
-    // empty variable for password
-    let password = "";
-
-    // loop length is password length from alert 1
-    for (var i = 1; i <= passLength; i++) {
-
-        // generated random integer value between 0 to length of the value
-        let randNumb = Math.floor(Math.random() * (value.length) - 1);
-        password = password + value.charAt(randNumb);
-        //console.log(password);
-    }
-
-    console.log(password);
-    document.getElementById('displayPass').innerHTML = password;
+function SyncAmount(e) {
+    const value = e.target.value
+    characterAmountNumbers.value = value
+    characterAmountrange.value = value
 }
